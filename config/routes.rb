4b10aba4815411281b2cc5985ff3aff20beb450c
Yasin <yasin@getvape.co.uk>
2017-11-14 21:27:43 +0000

    version one

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+root "pages#home"
+get "about" => "pages#about" #how to create a page
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
